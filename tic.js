5d81685da030f7983540991605e187f35c86a4dc
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-08-28 12:10:11 +0300

    UI player selection positioning updated

--- tic.js
+++ tic.js
@@ -1,16 +1,42 @@
-const calculator = (function(){
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-    return {
-      add,
-      sub,
-      mul,
-      div,
-    };
-  })();
+// const calculator = (function(){
+//     const add = (a, b) => a + b;
+//     const sub = (a, b) => a - b;
+//     const mul = (a, b) => a * b;
+//     const div = (a, b) => a / b;
+//     return {
+//       add,
+//       sub,
+//       mul,
+//       div,
+//     };
+//   })();
   
-  calculator.add(3,5); 
-  calculator.sub(6,2);
-  calculator.mul(14,5534);
+//   calculator.add(3,5); 
+//   calculator.sub(6,2);
+//   calculator.mul(14,5534);
+
+const player_x = (function(){
+    const selection = document.querySelector('.Player-x')
+    let playSymbol = "X"
+})();
+
+const player_o = (function(){
+  const selection = document.querySelector('.Player-o')
+  let playSymbol = "O"
+})();
+
+const Game = (function(){
+  return{
+
+  }
+})();
+var playSelection = function(choice){
+  return document.querySelector(`.${choice}`)
+} 
+  
+
+
+playSelection("one").addEventListener("click",()=>{
+  playSelection('one').textContent = "X"
+
+})
